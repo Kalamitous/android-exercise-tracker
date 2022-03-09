7a92317d214bcf055d52d5407dfd843ecac07bb1
Maya Raman <maya.rraman@gmail.com>
2022-03-08 21:24:46 -0800

    adding exercise menu

---
+++ app/src/main/java/com/example/androidexercisetracker/ExerciseMenu.java
@@ -0,0 +1,72 @@
+package com.example.androidexercisetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ExerciseMenu extends AppCompatActivity {
+    Button runningButton;
+    Button bikingButton;
+    Button rowingButton;
+    Button freeWeightsButton;
+    Button stairClimbingButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runningButton = (Button) findViewById(R.id.runningButton);
+        bikingButton = (Button) findViewById(R.id.bikingButton);
+        rowingButton = (Button) findViewById(R.id.rowingButton);
+        freeWeightsButton = (Button) findViewById(R.id.freeWeightsButton);
+        stairClimbingButton = (Button) findViewById(R.id.stairClimbingButton);
+
+        runningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRecordPage();
+            }
+        });
+
+        bikingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRecordPage();
+            }
+        });
+
+        stairClimbingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRecordPage();
+            }
+        });
+
+        freeWeightsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRecordPage();
+            }
+        });
+
+        rowingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToRecordPage();
+            }
+        });
+
+    }
+
+    //change to switch page on click
+    private void switchToRecordPage() {
+        //test purposes. delete this:
+        Toast successToast = Toast.makeText(this,  "Button pushed", Toast.LENGTH_SHORT);
+        successToast.show();
+
+    }
+}
